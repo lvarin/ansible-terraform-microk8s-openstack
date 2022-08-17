b454ef0152ef20090febf31c29aac81c4642f600
Alvaro Gonzalez <alvaro.gonzalez@csc.fi>
2022-08-17 15:21:30 +0300

    Set the device in the attachment declaration, so ansible can get a predictable device

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,6 +101,7 @@ resource "openstack_compute_instance_v2" "nfs" {
 resource "openstack_compute_volume_attach_v2" "va_1" {
   instance_id = "${openstack_compute_instance_v2.nfs.id}"
   volume_id   = "${openstack_blockstorage_volume_v2.volume_1.id}"
+  device      = "/dev/vdb"
 }
 
 # Create "instance_count" instances
